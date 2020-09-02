957cd3b3fa7140406d4ebcfb1ca3d2a10d942136
musashi-kitaura <dmchyd42@gmail.com>
2020-09-03 06:21:30 +0900

    commit

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,4 +1,15 @@
 class ShoppingsController < ApplicationController
 	def index
+		@shoppings = Shopping.all
+		@shopping = Shopping.new
+	end	
+
+	def create
+		Shopping.create(shopping_params)
+	end	
+
+	private 
+	def shopping_params
+		params.require(:shopping).permit(:name, :note)
 	end	
 end
