1b3fdd13f8c7d0191569a2035bf1cc220f029889
musashi-kitaura <dmchyd42@gmail.com>
2020-09-05 06:08:52 +0900

    commit

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -6,8 +6,15 @@ class ShoppingsController < ApplicationController
 
 	def create
 		Shopping.create(shopping_params)
+		redirect_to root_path
 	end	
 
+	def destroy
+		shopping = Shopping.find(params[:id])
+		shopping.destroy
+		redirect_to root_path
+	end	
+		
 	private 
 	def shopping_params
 		params.require(:shopping).permit(:name, :note)
